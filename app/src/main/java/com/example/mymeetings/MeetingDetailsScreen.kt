974e7a494b50d61d3c7be63a01dc5c05ab7186e5
Ann <annsvist@gmail.com>
2024-04-16 17:30:33 +1000

    Added UI elements to MeetingsListScreen and MeetingDetailsScreen

--- app/src/main/java/com/example/mymeetings/MeetingDetailsScreen.kt
+++ app/src/main/java/com/example/mymeetings/MeetingDetailsScreen.kt
@@ -1,19 +1,30 @@
 package com.example.mymeetings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
 fun MeetingDetailsScreen(onNavigateToClients: () -> Unit, modifier: Modifier = Modifier) {
+
+    val meetingTitle: String = "Title"
+    val meetingDateTime: String = "Date and Time"
 
     Column(
         modifier = modifier
@@ -26,8 +37,57 @@ fun MeetingDetailsScreen(onNavigateToClients: () -> Unit, modifier: Modifier = M
             text = "Meetings details",
             fontSize=25.sp
         )
-        Button(onClick = { onNavigateToClients() }) {
-            Text("Go to Clients list")
+        Column (modifier = modifier.padding(horizontal = 16.dp, vertical = 8.dp)) {
+            TextField(
+                value = meetingTitle,
+                onValueChange = {},
+                label = { Text("Meeting title")},
+                singleLine = true,
+                modifier = modifier.padding(vertical = 4.dp).fillMaxWidth()
+            )
+            TextField(
+                value = meetingDateTime,
+                onValueChange = {},
+                label = { Text("Meeting date and time")},
+                singleLine = true,
+                modifier = modifier.padding(vertical = 4.dp).fillMaxWidth()
+            )
+
+            PersonCard(onNavigateToClients = onNavigateToClients, modifier = modifier)
+
+        }
+    }
+}
+
+@Composable
+fun PersonCard(onNavigateToClients: () -> Unit, modifier: Modifier) {
+    val personName: String = "Person name"
+    val personEmail: String = "Person email"
+
+    Card (modifier = modifier
+        .padding(vertical = 4.dp)
+        .fillMaxWidth()
+    ) {
+        Column(modifier = modifier.fillMaxWidth()) {
+            Text(
+                text = "Person",
+                modifier = modifier.padding(start = 8.dp, top = 8.dp)
+            )
+            Row(modifier = modifier.padding(8.dp)) {
+                Box(
+                    modifier = modifier
+                        .size(60.dp)
+                        .background(Color.Green)
+                )
+                Column(modifier = modifier.padding(start = 8.dp)) {
+                    Text(text = personName)
+                    Text(text = personEmail)
+                }
+            }
+            Button(modifier = modifier.align(Alignment.CenterHorizontally),
+                onClick = { onNavigateToClients() }) {
+                Text("Select a person")
+            }
         }
     }
 }
--- app/src/main/java/com/example/mymeetings/MeetingsListScreen.kt
+++ app/src/main/java/com/example/mymeetings/MeetingsListScreen.kt
@@ -1,10 +1,14 @@
 package com.example.mymeetings
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
@@ -16,6 +20,11 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.ui.graphics.Color
 
 @Composable
 fun MeetingsListScreen(onNavigateToDetails: () -> Unit, modifier: Modifier = Modifier) {
@@ -38,26 +47,36 @@ fun MeetingsListScreen(onNavigateToDetails: () -> Unit, modifier: Modifier = Mod
             .fillMaxWidth()
         ) {
             items(meetingsVM.getMeetings()) { meeting ->
-                MeetingItem(meeting)
+                MeetingItem(meeting, modifier)
             }
         }
-        Button(onClick = { onNavigateToDetails() }) {
-            Text("Go to Meeting details")
+        FloatingActionButton(modifier = modifier
+            .padding(horizontal = 16.dp)
+            .align(Alignment.End),
+            onClick = { onNavigateToDetails() }) {
+            Icon(Icons.Filled.Add, contentDescription = "Add new meeting")
         }
     }
 
 }
 
 @Composable
-fun MeetingItem(item: Meeting) {
-    Card (modifier = Modifier
+fun MeetingItem(item: Meeting, modifier: Modifier) {
+    Card (modifier = modifier
+        .padding(vertical = 4.dp)
         .fillMaxWidth()
-        .padding(4.dp)
     ) {
-        Column {
-            Text(item.title)
-            Text(item.date)
-            Text(item.person)
+        Row(modifier = modifier.padding(8.dp)) {
+            Box(modifier = modifier
+                .size(60.dp)
+                .background(Color.Green)
+            )
+            Column (modifier = modifier.padding(start = 8.dp)) {
+                Text(item.title)
+                Text(item.date)
+                Text(item.person)
+            }
         }
+
     }
 }
